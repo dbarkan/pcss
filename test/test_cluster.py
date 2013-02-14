fe76b73d5d26711c48b3670dd10a9a8ddae1fd55
Dave Barkan <dbarkan@Daves-MacBook-Pro.local>
2013-02-13 21:56:54 -0800

    commit before trying out on cluster for the first time

---
+++ test/test_cluster.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import pcssTools
+import pcssCluster
+import unittest
+import configobj
+
+class TestCluster(unittest.TestCase):
+
+    def test_preprocess(self):
+
+        configFile = "testConfig/testPcssConfig.txt"
+        configSpecFile = "testConfig/testConfigSpec.txt"
+        self.pcssConfig = configobj.ConfigObj(configFile, configspec=configSpecFile)
+        self.pcssConfig['fasta_file'] = os.path.join(self.pcssConfig["pcss_directory"], "data", "inputSequences", "ffSequencesFasta.txt")
+
+        self.runner = pcssTools.PcssRunner(self.pcssConfig)
+        seqDivider = pcssCluster.SeqDivider(self.runner)
+        seqDivider.divideSeqsFromFasta(self.pcssConfig['fasta_file'])
+        seqDivider.makeBaseSgeScript()
+
+if __name__ == '__main__':
+    unittest.main()
