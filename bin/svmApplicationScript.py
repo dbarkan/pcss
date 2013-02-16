e93ab7ae8583e8c8e73b2506e027facdb7feabe5
Dave Barkan <davebarkan@gmail.com>
2013-02-15 16:04:04 -0800

    more configspec

--- bin/svmApplicationScript.py
+++ bin/svmApplicationScript.py
@@ -8,7 +8,7 @@ import os
 
 configFileName = sys.argv[1]
 tempPcssConfig = configobj.ConfigObj(configFileName)
-configSpec = tempPcssConfig["config_spec_file"]
+configSpec = tempPcssConfig["user_config_spec_file"]
 
 pcssConfig = configobj.ConfigObj(configFileName, configspec=configSpec)
 pcssConfig["attribute_file_name"] = os.path.join(pcssConfig["pcss_directory"], "data", "context", "svmApplicationFileAttributes.txt")
