322e6ab6a9db89900724bc3ac007af0ed3a7b959
Dave Barkan <davebarkan@gmail.com>
2013-02-18 21:55:01 -0800

    commit cluster training annotation

---
+++ bin/clusterExe/svmTrainingAnnotationScript.py
@@ -0,0 +1,16 @@
+import pcssTools
+import pcssCluster
+import configobj
+
+import sys
+import os
+
+
+configFileName = sys.argv[1]
+tempPcssConfig = configobj.ConfigObj(configFileName)
+configSpec = tempPcssConfig["user_config_spec_file"]
+
+pcssConfig = configobj.ConfigObj(configFileName, configspec=configSpec)
+pcssConfig["attribute_file_name"] = os.path.join(pcssConfig["pcss_directory"], "data", "context", "svmTrainingAttributes.txt")
+runner = pcssTools.AnnotationRunner(pcssConfig)
+runner.execute()
